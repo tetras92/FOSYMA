7a33297de27dc2270cc33dcd73e286bbdf725e87
tetras92 <manuelamous@gmail.com>
2018-02-22 19:12:06 +0100

    aa

---
+++ jade/src/main/java/agents/exercice3/AgentSender.java
@@ -0,0 +1,12 @@
+package agents.exercice3;
+
+import jade.core.Agent;
+
+public class AgentSender extends Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2840737068182272358L;
+
+}
